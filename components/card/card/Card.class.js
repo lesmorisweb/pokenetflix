859bfcb8e365d1ac77e4e03cb0ad55d82760a524
Laura Esmoris <lesmoris01@gmail.com>
2022-05-17 21:59:14 -0500

    card classes in progress

---
+++ components/card/card/Card.class.js
@@ -0,0 +1,24 @@
+export class Card {
+   constructor(id, image, name) {
+      this.id = id;
+      this.image = image;
+      this.name = name;
+      
+      this.display();
+      
+      }
+   
+   display() {
+      // Construir el componente en html
+      // .............
+      
+   }
+
+   displayPokemonInfo(){
+      
+   }
+   
+}
+
+
+
