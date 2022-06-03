88c32bfdf9c5dd92a88cbac854130adbf051ad52
josedavms13 <josedav0ms13@gmail.com>
2022-06-02 22:14:03 -0500

    Card Page build

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { Pokeapi } from "./services/Pokeapi.class.js";
 import { MainPage } from "./components/mainPage/MainPage.class.js";
 
 const pokeApi = new Pokeapi();
-const pokeTypes = await pokeApi.getAllTypes();
 
-const mainPage = new MainPage(pokeTypes.results);
+const pokeTypes = await pokeApi.getAllTypes();
+const pokeArray = await pokeApi.getPokemonsInfoByType("fire");
+
+const mainPage = new MainPage(pokeTypes.pokemon, pokeArray);
--- services/Pokeapi.class.js
+++ services/Pokeapi.class.js
@@ -1,44 +1,64 @@
 export class Pokeapi {
 
-   queryParam = "";
-   baseURL= "https://pokeapi.co/api/v2"
+    queryParam = "";
+    baseURL = "https://pokeapi.co/api/v2"
 
-   headers = {
-      "content-type": "json"
-   }
+    headers = {
+        "content-type": "json"
+    }
 
-   config = {
-      method: "GET",
-      headers: this.headers
-   }
+    config = {
+        method: "GET", headers: this.headers
+    }
 
-   getPokemonsByType(type) {
-      this.queryParam = "type";
-      const request = new Request(`${this.baseURL}/${this.queryParam}/${type}`, this.config);
+    getPokemonsByType(type) {
+        this.queryParam = "type";
+        const request = new Request(`${this.baseURL}/${this.queryParam}/${type}`, this.config);
 
-      return fetch(request)
-         .then((respuesta) => respuesta.json())
-   }
+        return fetch(request)
+            .then((respuesta) => respuesta.json())
+    }
 
-   getAllTypes() {
-      this.queryParam = "type";
-      console.log(`${this.baseURL}/${this.queryParam}`)
-      const typeRequest = new Request(`${this.baseURL}/${this.queryParam}`, this.config);
+    getAllTypes() {
+        this.queryParam = "type";
+        console.log(`${this.baseURL}/${this.queryParam}`)
+        const typeRequest = new Request(`${this.baseURL}/${this.queryParam}`, this.config);
 
-      return fetch(typeRequest)
-         .then((respuesta) => respuesta.json())
-   }
+        return fetch(typeRequest)
+            .then((respuesta) => respuesta.json())
+    }
 
-   getPokemoByName(name) {
-      this.queryParam = "pokemon"
-      const request = new Request(`${this.baseURL}/${this.queryParam}/${name}`, this.config);
-      return fetch(request)
-         .then((respuesta) => respuesta.json())
-   }
+    getPokemoByName(name) {
+        this.queryParam = "pokemon"
+        const request = new Request(`${this.baseURL}/${this.queryParam}/${name}`, this.config);
+        return fetch(request)
+            .then((respuesta) => respuesta.json())
+    }
+
+    getPokemonByUrl(url) {
+        const request = new Request(url, this.config);
+        return fetch(request)
+            .then(res => res.json());
+    }
+
+    async getPokemonsInfoByType(type) {
+        const pokeInfo = await this.getPokemonsByType(type);
+        const pokeUrls = pokeInfo.pokemon.map(data => data.pokemon.url);
+
+        const pokeArray = [];
+
+        let result;
+        //For of porque es async
+        for (const url of pokeUrls) {
+            result = await this.getPokemonByUrl(url)
+            pokeArray.push(result)
+        }
+        return pokeArray;
+    }
 }
 
 ///  https://www.pokeapi.co/api/v2/element/rock
-///  {         base }        queryparam}{item} 
+///  {         base }        queryparam}{item}
 
 
 
